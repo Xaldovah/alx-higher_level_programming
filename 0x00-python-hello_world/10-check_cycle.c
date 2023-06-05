611088a00072b250bbde4ede2afe1dd8da1d7549
Dennis Kimani <dxaldovah@gmail.com>
2023-06-05 13:30:19 -0700

    Removed unnecessary lines to make the code more simplified

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,32 +2,23 @@
 #include <stdio.h>
 
 /**
- * check_cycle - This func checks if a singly linked list
- * has a cycle in it
- * @list: The list to check
+ * check_cycle - Checks if a singly linked list has a cycle in it.
+ * @list: The list to check.
  *
- * Return: 0 if there is no cycle, 1 if there is a cycle
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *cheetah = list, *snail = list;
-	int found = 0;
+	listint_t *cheetah = list;
+	listint_t *snail = list;
 
-	while ((cheetah && snail) && snail->next)
+	while (cheetah && snail && snail->next)
 	{
 		snail = snail->next;
+		cheetah = cheetah->next->next;
 
-		if (cheetah->next || cheetah->next->next)
-			cheetah = cheetah->next->next;
-		else
-		{
-			break;
-		}
 		if (snail == cheetah)
-		{
-			found = 1;
-			break;
-		}
+			return 1;
 	}
-	return (found);
+	return 0;
 }
