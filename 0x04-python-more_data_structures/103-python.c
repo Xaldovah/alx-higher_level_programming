7faea77cadeb6886c0eb63e71f1c2d259b389083
Dennis Kimani <dxaldovah@gmail.com>
2023-06-14 15:00:24 -0700

    Create two C functions that print some basic info about Python lists and Python bytes objects

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,21 +1,42 @@
 #include "/usr/include/python3.4/Python.h"
-#include <stdion.h>
+#include <stdio.h>
+#include <Python.h>
+#include <stdlib.h>
 
-void print_python_list(PyObject *p) {
-    Py_ssize_t size;
-    PyObject *item;
-    size = PyList_Size(p);
-    printf("list size: %zd\n", size);
-    for (int i = 0; i < size; i++) {
-        item = PyList_GetItem(p, i);
-        printf("item %d: %s\n", i, Py_TYPE(item)->tp_name);
+void print_python_list(PyObject *p)
+{
+    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t i;
+
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; i++)
+    {
+        printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
     }
 }
-void print_python_bytes(PyObject *p) {
-    Py_ssize_t size;
-    const char *bytes;
-    size = PyBytes_GET_SIZE(p);
-    bytes = PyBytes_AS_STRING(p);
-    printf("bytes size: %zd\n", size);
-    printf("first 10 bytes: %.10s\n", bytes);
+
+void print_python_bytes(PyObject *p)
+{
+    PyBytesObject *bytes = (PyBytesObject *)p;
+    Py_ssize_t size, i;
+
+    printf("[.] bytes object info\n");
+    if (!PyBytes_Check(p))
+    {
+        printf("  [ERROR] Invalid Bytes Object\n");
+        return;
+    }
+
+    size = PyBytes_Size(p);
+    printf("  size: %ld\n", size);
+    printf("  trying string: %s\n", PyBytes_AS_STRING(p));
+    printf("  first %ld bytes:", (size < 10) ? size + 1 : 10);
+    for (i = 0; i < size && i < 10; i++)
+    {
+        printf(" %02x", bytes->ob_sval[i] & 0xff);
+    }
+    printf("\n");
 }
