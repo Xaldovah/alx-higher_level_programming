ee8e826b4816955fc452092fbb19d7ff098b7e7f
Dennis Kimani <dxaldovah@gmail.com>
2023-06-26 15:53:21 -0700

    Python function def magic_calculation(a, b)

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 import dis
 
+
 def magic_calculation(a, b):
-    res = 0
+    result = 0
 
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception("Out of Range")
-            res += a ** b / i
+                raise Exception("Far Off")
+
+            result += a ** b / i
         except:
-            res = b + a
+            result = b + a
             break
-        return res
+
+    return result
