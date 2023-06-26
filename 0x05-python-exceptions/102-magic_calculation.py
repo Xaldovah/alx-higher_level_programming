8ced49641d9c54ba1b9827ab3d3c6a25a4a74887
Dennis Kimani <dxaldovah@gmail.com>
2023-06-26 16:01:09 -0700

    Python function def magic_calculation(a, b)

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -11,7 +11,7 @@ def magic_calculation(a, b):
                 raise Exception("Far Off")
 
             result += a ** b / i
-        except (ValueError, IndexError, ZeroDivisionError):
+        except Exception:
             result = b + a
             break
 
