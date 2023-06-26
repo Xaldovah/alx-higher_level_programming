4420f2c6754027d3214929169fa9ace616b70ec5
Dennis Kimani <dxaldovah@gmail.com>
2023-06-26 15:31:05 -0700

    function that executes a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except ZeroDivisionError:
+        result = None
+        sys.stderr.write("Exception: zero division\n")
+    except IndexError:
+        result = None
+        sys.stderr.write("Exception: index too long\n")
+
+    return result
