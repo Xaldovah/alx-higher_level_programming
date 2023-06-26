04daabf34f917a498b16411ee39cd96090c4af78
Dennis Kimani <dxaldovah@gmail.com>
2023-06-26 15:08:52 -0700

    PEP8 correction

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@
 
 def safe_print_division(a, b):
     result = 0
-    
+
     try:
         result = a / b
     except ZeroDivisionError:
@@ -11,4 +11,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(result))
         return result
-
