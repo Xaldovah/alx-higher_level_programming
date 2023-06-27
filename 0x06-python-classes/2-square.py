5111aea4f8d58f1774047a067ddbb162db8f9dba
Dennis Kimani <dxaldovah@gmail.com>
2023-06-27 16:32:42 -0700

    PEP 8 Correction

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,26 +7,15 @@ A Square Class
 
 
 class Square:
-
+    """__init__ method initializes the size value of the square."""
     def __init__(self, size=0):
-        """__init__
-
-        The __init__ method initializes the size value of the square.
-        """
-
+        """Attributes: size: This is the size of the square"""
         if type(size) is not int:
-            """Attributes:
-                size: This is the size of the square
-            """
+            """Raise: TypeError: Is raised if size type is not `int`"""
             raise TypeError("size must be an integer")
-            """Raise:
-                TypeError: Is raised if size type is not `int`
-            """
-
+        """Check if size is than `0`"""
         if size < 0:
+            """Raise: ValueError: Is raised if size is than `0`"""
             raise ValueError("size must be >= 0")
-            """Raise:
-                ValueError: Is raised if size is less than 0
-            """
-
+        """size: data assigning"""
         self.__size = size
