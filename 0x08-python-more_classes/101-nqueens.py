0c3ca843a2f44ca246820166307b307ea27dc1f8
Dennis Kimani <dxaldovah@gmail.com>
2023-07-03 15:27:43 -0700

    a program that solves the N queens problem

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -3,17 +3,17 @@ import sys
 
 
 def is_safe(bd, row, col):
-    # Check if a queen can be placed at board[row][col] position
+    """Check if a queen can be placed at board[row][col] position"""
     for i in range(row):
-        # Check if there is a queen in the same column or diagonals
+        """Check if there is a queen in the same column or diagonals"""
         if bd[i] == col or bd[i] - col == i - row or bd[i] - col == row - i:
             return False
     return True
 
 
 def solve_nqueens(bd, row, n, solutions):
+    """Add the solution to the list"""
     if row == n:
-        # Add the solution to the list
         solutions.append([[i, bd[i]] for i in range(n)])
         return
 
@@ -25,7 +25,7 @@ def solve_nqueens(bd, row, n, solutions):
 
 
 def output(P, c):
-    for solution in c:
+    for solution in P:
         print(solution)
 
 
@@ -34,29 +34,26 @@ def nqueens(n):
         print("N must be a number")
         sys.exit(1)
     if n < 4:
-        print("N must be at least 4")
+        print("N must be greater than 4")
         sys.exit(1)
 
-    bd = [-1] * n  # Initialize an empty chessboard
     solutions = []
-
+    bd = [-1] * n
     solve_nqueens(bd, 0, n, solutions)
-
-    output(None, solutions)
+    output(solutions, n)
 
 
 def main():
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
     try:
-        N = int(sys.argv[1])
-        nqueens(N)
+        n = int(sys.argv[1])
     except ValueError:
         print("N must be an integer")
         sys.exit(1)
+    nqueens(n)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
