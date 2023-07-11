f3abc8420f8271b9c369047278f04a7482b7a680
Dennis Kimani <dxaldovah@gmail.com>
2023-07-11 09:59:49 -0700

    class Student that defines a student by: (based on 9-student.py)

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""class Student module"""
+
+class Student:
+    """class Student"""
+    def __init__(self, first_name, last_name, age):
+        """init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """to_json method"""
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        """to_json method"""
+        if type(attrs) is list:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
