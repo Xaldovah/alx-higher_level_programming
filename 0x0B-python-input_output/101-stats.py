7b5fda2d1cc0bcd6eb35fa97026b682f5d97bad4
Dennis Kimani <dxaldovah@gmail.com>
2023-07-12 03:13:03 -0700

    script that reads stdin line by line and computes metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,33 +1,53 @@
 #!/usr/bin/python3
-"""print_stats function module"""
-
+"""import module"""
 import sys
 
+"""print_info function module"""
 
-def print_stats(total_size, status_codes):
-    """print_stats function"""
-    print("File size: {}".format(total_size))
-    sorted_codes = sorted(status_codes.keys())
-    for code in sorted_codes:
-        print("{}: {}".format(code, status_codes[code]))
+def print_stats():
+    print("File size: {:d}".format(file_size))
 
+    for scode, code_times in sorted(status_codes.items()):
+        if code_times > 1:
+            print("{}: {:d}".format(scode, code_times))
 
-total_size = 0
-status_codes = {}
+status_codes = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0,
+}
+
+file_size = 0
+iterator = 0
 
 try:
-    for i, line in enumerate(sys.stdin, 1):
-        line = line.strip()
-        data = line.split(" ")
-        if len(data) >= 7:
-            size = int(data[-1])
-            code = data[-2]
-            total_size += size
-            if code in status_codes:
-                status_codes[code] += 1
-            else:
-                status_codes[code] = 1
-        if i % 10 == 0:
-            print_stats(total_size, status_codes)
+    for line in sys.stdin:
+        if iterator != 0 and iterator % 10 == 0:
+            print_stats()
+        
+        pieces = line.split()
+
+        try:
+            status = int(pieces[-2])
+
+            if str(status) in status_codes.keys():
+                status_codes[str(status)] += 1
+        except:
+            pass
+
+        try:
+            file_size += int(pieces[-1])
+        except:
+            pass
+
+        iterator += 1
+    
+    print_stats()
 except KeyboardInterrupt:
-    print_stats(total_size, status_codes)
+    print_stats()
+    raise
