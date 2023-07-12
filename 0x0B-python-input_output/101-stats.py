79de3bfb98d573cae0394d72ebb5c8b0462785cf
Dennis Kimani <dxaldovah@gmail.com>
2023-07-12 03:23:28 -0700

    script that reads stdin line by line and computes metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -36,12 +36,12 @@ try:
 
             if str(status) in status_codes.keys():
                 status_codes[str(status)] += 1
-        except ValueError:
+        except Exception:
             pass
 
         try:
             file_size += int(pieces[-1])
-        except ValueError:
+        except Exception:
             pass
 
         it += 1
