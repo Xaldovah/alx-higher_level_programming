e170af6970470fe9e5a6a3be37cc37a2ca447a08
Dennis Kimani <dxaldovah@gmail.com>
2023-07-14 13:12:22 -0700

    Update the class Square by adding the public method def update(self, *args, **kwargs) that assigns attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#/usr/bin/python3
 """import Rectangle class module"""
 from models.rectangle import Rectangle
 """Square class module"""
@@ -10,6 +10,31 @@ class Square(Rectangle):
         """Constructor"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """String representation"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def update(self, *args, **kwargs):
+        """Update"""
+        if args and len(args) > 0:
+            attrs = ["id", "size", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attrs[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
