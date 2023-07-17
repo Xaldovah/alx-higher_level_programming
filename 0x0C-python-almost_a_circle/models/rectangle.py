a1cfab837a12ecbabff958127e6800ba8021a40a
Dennis Kimani <dxaldovah@gmail.com>
2023-07-17 09:58:56 -0700

    test_rectangle fixes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,7 +82,9 @@ class Rectangle(Base):
 
     def __str__(self):
         """__str__"""
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return '[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}'.format(
+            self.id, self.x, self.y, self.width, self.height
+        )
 
     def update(self, *args, **kwargs):
         """update keys"""
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,113 +1,92 @@
 #!/usr/bin/python3
-"""import modules"""
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
-import json
 import pep8
+import json
 
 
 class TestRectangleClass(unittest.TestCase):
-    """Test the class Rectangle"""
 
     @classmethod
     def setUp(cls):
-        """setUp"""
         Base._Base__nb_objects = 0
-        cls.rect1 = Rectangle(1, 3)
-        cls.rect2 = Rectangle(1, 3, 5)
-        cls.rect3 = Rectangle(1, 3, 5, 7)
-        cls.rect4 = Rectangle(1, 3, 5, 7, 9)
+        cls.rect1 = Rectangle(2, 4)
+        cls.rect2 = Rectangle(2, 4, 6)
+        cls.rect2 = Rectangle(2, 4, 6, 8)
+        cls.rect2 = Rectangle(2, 4, 6, 8, 10)
 
     @classmethod
-    def tearDown(cls):
-        """tearDown"""
+    def tearDownClass(self):
         pass
 
-    def test_pep8_conformance(self):
-        """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/rectangle.py'])
-        self.assertEqual(result.total_errors, 0, "Pep8 Error in file")
-
-    def test_is_subclass(self):
-        """Test that Rectangle is a subclass of Base."""
-        self.assertTrue(issubclass(Rectangle, Base))
-
-    def test_instance(self):
-        """Test that Rectangle is an instance of Base."""
-        self.assertIsInstance(self.rect1, Base)
-        self.assertIsInstance(self.rect2, Base)
-        self.assertIsInstance(self.rect3, Base)
-        self.assertIsInstance(self.rect4, Base)
-
-    def test_save_to_file(self):
-        """Test the save_to_file method of Rectangle"""
-        Rectangle.save_to_file(None)
-        with open("Rectangle.json", "r") as f:
-            self.assertEqual("[]", f.read())
-
-    def test_save_load_file(self):
-        """Test the save_to_file and load_from_file methods of Rectangle"""
-        rect1 = Rectangle(4, 6, 2, 8)
-        rect2 = Rectangle(7, 9)
-        Rectangle.save_to_file([rect1, rect2])
-        load_file = Rectangle.load_from_file()
-        self.assertEqual(isinstance(load_file, list))
-
-    def test_to_dict(self):
-        """Test the to_dict method of Rectangle."""
-        dict1 = self.rect1.to_dictionary()
-        self.assertEqual(
-            {"id": 1, "width": 2, "height": 3, "x": 0, "y": 0}, dict1)
-
     def test_class(self):
-        """Test the class of Rectangle."""
+        """Rectangle is a class"""
         self.assertTrue(str(Rectangle), "<class 'models.rectangle.Rectangle'>")
 
+    def test_B_inheritance(self):
+        """Tests if Rectangle inherits Base."""
+        self.assertTrue(issubclass(Rectangle, Base))
+
+    def test_id_4(self):
+        """Tests id"""
+        b1 = Base(1)
+        b2 = Base(2)
+        b3 = Base(3)
+        b4 = Base(69)
+        b5 = Base(4)
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+        self.assertEqual(b4.id, 69)
+        self.assertEqual(b5.id, 4)
+
+    def test_id(self):
+        """Tests id"""
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 4)
+        r2 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r2.id, 12)
+
+    def test_to_dict(self):
+        """Tests dict"""
+        dict1 = self.rect1.to_dictionary()
+        self.assertEqual(
+            {"x": 0, "y": 0, "id": 1, "height": 4, "width": 2}, dict1)
+
     def test_area(self):
-        """Test the area method of Rectangle."""
-        rect1 = Rectangle(10, 10, 10, 10, 42)
-        self.assertEqual(str(rect1), "[Rectangle] (42) 10/10 - 10/10")
+        """Tests area"""
+        r1 = Rectangle(10, 10, 10, 10, 42)
+        self.assertEqual(str(r1), "[Rectangle] (42) 10/10 - 10/10")
 
     def test_update(self):
-        """Test the update method of Rectangle."""
-        rect1 = {"id": 1, "width": 2, "height": 3, "x": 0, "y": 0}
-        rect2 = {"id": 2, "width": 4, "height": 5, "x": 1, "y": 2}
-        rect_create1 = Rectangle.create(**rect1)
-        rect_create2 = Rectangle.create(**rect2)
-        self.assertEqual("[Rectangle] (1) 0/0 - 2/3", str(rect_create1))
-        self.assertEqual("[Rectangle] (2) 1/2 - 4/5", str(rect_create2))
+        """Tests the update"""
+        rect1 = {"id": 42, "width": 1, "height": 2, "x": 3, "y": 4}
+        rect2 = {"id": 23, "width": 4, "height": 3, "x": 5, "y": 12}
+        rdct_create1 = Rectangle.create(**rect1)
+        rdct_create2 = Rectangle.create(**rect2)
+        self.assertEqual("[Rectangle] (42) 3/4 - 1/2", str(rdct_create1))
+        self.assertEqual("[Rectangle] (23) 5/12 - 4/3", str(rdct_create2))
         self.assertTrue(isinstance(rect1, dict))
         self.assertTrue(isinstance(rect2, dict))
 
     def test_save_to_file(self):
-        """Test the save_to_file method of Rectangle"""
+        """Tests save to file"""
         Rectangle.save_to_file(None)
         with open("Rectangle.json", "r") as f:
             self.assertEqual("[]", f.read())
 
     def test_save_load_file(self):
-        """Test the save_to_file and load_from_file methods of Rectangle"""
-        rect1 = Rectangle(4, 6, 2, 8)
-        rect2 = Rectangle(7, 9)
-        Rectangle.save_to_file([rect1, rect2])
+        """Tests save load file"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
         load_file = Rectangle.load_from_file()
-        self.assertEqual(isinstance(load_file, list))
+        self.assertTrue(isinstance(load_file, list))
 
     def test_args(self):
-        """Test the args method of Rectangle."""
+        """Tests args"""
         with self.assertRaises(TypeError):
             rect1 = Rectangle()
             self.rect1.area(1)
-
-    def test_id(self):
-        """Test the id method of Rectangle."""
-        rect1 = Rectangle(1, 2)
-        self.assertEqual(rect1.id, 1)
-        rect2 = Rectangle(2, 3, 4, 5)
-        self.assertEqual(rect2.id, 2)
-
-if __name__ == '__main__':
-    unittest.main()
