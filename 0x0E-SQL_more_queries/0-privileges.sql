1a4e47707e2c4a5fd020fdc0f49808353deec197
xaldovah <dxaldovah@gmail.com>
2023-08-16 12:31:24 +0000

    script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- lists all privileges
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
+USE mysql;
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
