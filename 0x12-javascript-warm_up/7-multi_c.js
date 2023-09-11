a427fc39d9f2fa65d5596f946e3f364b12d526a4
Dennis Kimani <dxaldovah@gmail.com>
2023-09-11 07:06:34 -0700

    script that prints x times C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const num = parseInt(process.argv[2]);
 
 if (!isNaN(num) && num > 0) {
   for (let i = 0; i < num; i++) {
-    console.log('c is fun');
+    console.log('C is fun');
   }
 } else if (isNaN(num)) {
   console.log('Missing number of occurrences');
