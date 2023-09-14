29660f6febe88e39bf7007eb8c8927724f4b3746
xaldovah <dxaldovah@gmail.com>
2023-09-14 23:28:45 +0000

    script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Connects to a MySQL database and retrieves data from the 'states' table.
+
+    Command-line arguments:
+    - argv[1]: MySQL username
+    - argv[2]: MySQL password
+    - argv[3]: MySQL database name
+
+    Prints the retrieved data to the console.
+    """
+
+    db = MySQLdb.connect(
+            host="localhost",
+            user=argv[1],
+            port=3306,
+            passwd=argv[2],
+            db=argv[3]
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+if __name__ == '__main__':
+    main()
