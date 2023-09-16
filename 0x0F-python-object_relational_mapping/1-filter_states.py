4f5a2aea07c445fd49278e20680207361d4a39a9
Dennis Kimani <dxaldovah@gmail.com>
2023-09-16 13:36:53 -0700

    script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,14 +17,37 @@ if __name__ == '__main__':
 
     Prints the retrieved data to the console.
     """
-    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    rows = cur.fetchall()
-
-    for row in rows:
-        if row[1].startswith('N'):
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=database,
+            port=3306
+        )
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' \
+                ORDER BY id ASC")
+        rows = cur.fetchall()
+        for row in rows:
             print(row)
 
-    cur.close()
-    db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+
+    finally:
+        if cur:
+            cur.close()
+        if db:
+            db.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    filter_states(username, password, database)
