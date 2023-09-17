9308ec98cb34a36dc46aad8fe670d0df6a326c40
Dennis Kimani <dxaldovah@gmail.com>
2023-09-17 15:17:13 -0700

    all the files that I created locally

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''
+"""
 Class definition of a State and an instance Base = declarative_base():
-'''
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,10 +9,10 @@ Base = declarative_base()
 
 
 class State(Base):
-    '''
+    """
     State class inherits from Base
-    '''
-    __tablename__ = 'states'
+    """
+    __tablename__ = "states"
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
