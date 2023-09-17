24d34b7665a214d1631139276919568bcf84ad98
Dennis Kimani <dxaldovah@gmail.com>
2023-09-17 15:35:33 -0700

    script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
- takes in the name of a state as an argument and
- lists all cities of that state, using the database hbtn_0e_4_usa
+Takes in the name of a state as an argument and
+lists all cities of that state using the database hbtn_0e_4_usa
 """
 
 import MySQLdb
@@ -9,26 +9,34 @@ from sys import argv
 
 if __name__ == '__main__':
     """
-    Connects to a MySQL database and retrieves data from the 'states' table.
+    Connects to a MySQL database and retrieves data from the 'cities'
+    and 'states' tables.
+    Joins the tables to display city names of the specified state.
 
     Command-line arguments:
     - argv[1]: MySQL username
     - argv[2]: MySQL password
     - argv[3]: MySQL database name
+    - argv[4]: State name
 
     Prints the retrieved data to the console.
     """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
 
     with db.cursor() as cur:
         cur.execute("""
-            SELECT cities.id, cities.name, states.name FROM cities
-            JOIN states ON cities.state_id = state.id
-            WHERE states.name LIKE BINARY %(state_name)s
-            ORDER BY cities.id ASC""", {'state_name': argv[4]})
+            SELECT cities.name FROM cities
+            JOIN states ON cities.state_id = states.id
+            WHERE states.name = %s
+            ORDER BY cities.id ASC""", (argv[4],))
 
         rows = cur.fetchall()
 
-    if rows is not None:
-        print(", ".join([row[1] for row in rows]))
+    if rows:
+        city_names = [row[0] for row in rows]
+        print(", ".join(city_names))
