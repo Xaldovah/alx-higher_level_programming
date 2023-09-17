652bed8bb8033418b74ee82a5c786ad2def7cb41
Dennis Kimani <dxaldovah@gmail.com>
2023-09-17 15:30:42 -0700

    script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name
-matches the argument
+Lists all cities from the database hbtn_0e_4_usa
 """
 
 import MySQLdb
@@ -10,7 +8,10 @@ from sys import argv
 
 if __name__ == '__main__':
     """
-    Connects to a MySQL database and retrieves data from the 'states' table.
+    Connects to a MySQL database and retrieves data
+    from the 'cities' and 'states' tables.
+    Joins the tables to display city names along with
+    their respective state names.
 
     Command-line arguments:
     - argv[1]: MySQL username
@@ -19,15 +20,23 @@ if __name__ == '__main__':
 
     Prints the retrieved data to the console.
     """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
 
     with db.cursor() as cur:
         cur.execute("""
             SELECT cities.id, cities.name, states.name FROM cities
-            JOIN states ON cities.state_id = state.id
+            JOIN states ON cities.state_id = states.id
             ORDER BY cities.id ASC""")
         rows = cur.fetchall()
-    if rows is not NONE:
+
+    if rows is not None:
         for row in rows:
             print(row)
+
+    cur.close()
+    db.close()
