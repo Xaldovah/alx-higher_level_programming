7e5626d965b370fdc1860f238ba1735bf803fe8d
Dennis Kimani <dxaldovah@gmail.com>
2023-09-21 09:52:23 -0700

    correction of the model_city file

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """
-Class definition of a City and
-an instance Base = declarative_base()
+Class definition of a City
 """
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
-
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
 
 class City(Base):
     """
-    City class inherits from Base
+    Represents a City
     """
-    __tablename__ = "cities"
-
-    id = Column(Integer, primary_key=True, nullable=False)
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
