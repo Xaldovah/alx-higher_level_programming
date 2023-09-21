cdc0513728b27da462829ae97e4d3ea1fa2c84c4
Dennis Kimani <dxaldovah@gmail.com>
2023-09-21 15:38:46 -0700

    script that creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa: (100-relationship_states_cities.py)

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -2,19 +2,17 @@
 """
 Class definition of a City
 """
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
-
-Base = declarative_base()
+from relationship_state import Base
 
 
 class City(Base):
     """
     Represents a City
     """
-        __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True, nullable=False)
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+python file that contains the class definition of a State
+and an instance Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    inherits from Base
+    links to the MySQL table states
+    class attribute id that represents a column of an auto-generated,
+    unique integer, can’t be null and is a primary key
+    class attribute name that represents a column of a string
+    with maximum 128 characters and can’t be null
+    """
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
