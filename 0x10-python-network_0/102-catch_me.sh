167681dabe164edceb8015692f2af8c3ed6a9323
Dennis Kimani <dxaldovah@gmail.com>
2023-09-28 13:07:31 -0700

    you got me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script makes a request to the specified URL, causing the server to respond with "You got me!"
-curl -s -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" "$1"
+curl -s http://0.0.0.0:5000/catch_me | grep "You got me!"
