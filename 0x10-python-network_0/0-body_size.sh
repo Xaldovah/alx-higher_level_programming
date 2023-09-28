59026cc7cae4baffb83398cf16c9211004cd3d7c
Dennis Kimani <dxaldovah@gmail.com>
2023-09-28 06:31:28 -0700

    Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# takes in a URL, sends a request to that URL, displays the size of the body of the response
+curl -sI "$1" | grep -i "Content-Length:" | awk '{print $2}'
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,1 @@
+This repository contains all tasks on python network
