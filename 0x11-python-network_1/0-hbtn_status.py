8a9df0a5ca59c688aea265f860092fe37521030b
Dennis Kimani <dxaldovah@gmail.com>
2023-09-29 03:10:53 -0700

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
         utf8_content = content.decode('utf-8')
 
         print('Body response:')
-        print('    - type:', type(content))
-        print('    - content:', content)
-        print('    - utf8 content:', utf8_content)
+        print('\t- type:', type(content))
+        print('\t- content:', content)
+        print('\t- utf8 content:', utf8_content)
