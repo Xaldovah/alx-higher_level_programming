9781f4080cc0e4ffe7acbb13b7e9c97fa529efe2
Dennis Kimani <dxaldovah@gmail.com>
2023-09-29 13:16:22 -0700

    takes in a URL, sends a request to the URL and displays the body of the response

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""takes in a URL, sends a request to the URL
+and displays the body of the response
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+
+    if req.status_code >= 400:
+        print('Error code:', req.status_code)
+    else:
+        print(req.text)
