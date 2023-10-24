fdcf4c4c4de72206aa05cfdcd576fc0ffeafb717
Dennis Kimani <dxaldovah@gmail.com>
2023-10-24 09:38:00 -0700

    script that writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,13 +5,10 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-if (!filePath || !content) {
-  console.error('Usage:node 1-writeme.js <file_path> <content>');
-  process.exit(1);
 }
 
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
-    console.error(err);
+    console.log(err);
   }
 });
