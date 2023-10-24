0e510b5ebb9fd75031db729a2a28cbd3c2ce5475
Dennis Kimani <dxaldovah@gmail.com>
2023-10-24 14:26:41 -0700

    script that display the status code of a GET request

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,9 +3,13 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(apiUrl, (error, response, body) => {
-	movieData = JSON.parse(body);
-	console.log(movieData.title);
+  if (!error && response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
+  } else {
+    console.error('Error:', error);
+  }
 });
