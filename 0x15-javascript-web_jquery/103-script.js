e003fc1131f1505c03520737b64800cfa5a1fc2a
Dennis Kimani <dxaldovah@gmail.com>
2023-11-06 06:59:12 -0800

    script that fetches and prints how to say “Hello” depending on the language

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+	$('#btn_translate').click(fetchTranslation);
+	$('#language_code').on('keypress', function (e) {
+		if (e.which === 13) {
+			fetchTranslation();
+		}
+	});
+
+	function fetchTranslation() {
+		const languageCode = $('#language_code').val();
+		$.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`, function (data) {
+			$('#hello').text(data.hello);
+		});
+	}
+});
